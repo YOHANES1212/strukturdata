26281add16c896d5140d715c36d83ecee131ff00
Yohanes Guido Bogin <guidoboginbogin@gmail.com>
2025-05-16 20:52:10 +0700

    menambahkan src

---
+++ pert7/src/database/seeders/BookSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Book;
+
+class BookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        Book::firstOrCreate([
+            'name' => 'Jungle Land',
+            'code_book' => '123',
+            'price' => 10
+        ]);
+    }
+}
